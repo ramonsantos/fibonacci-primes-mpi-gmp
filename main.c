0bc0bf9e91dcbc060a84d503a8430a64b32c6178
Ramon Santos <ramonsantos.pe@gmail.com>
2014-08-17 15:55:44 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <mpi.h>
 #include <gmp.h>
-#include<time.h>
+
 #define PRECISAO 25
-int main(){
+
+int rank, size;
+
+int main(int argc, char* argv[]){
     
     //Arquivo com os números encontrados.
     FILE* resultado;
@@ -16,33 +20,44 @@ int main(){
     mpz_init_set_str(ultm, "1", 10);
     mpz_init_set_str(penl, "1", 10);
     
-    int encontrados = 0;
+    // 'n'º número de Fibonacci encontrado.
+    int n_fib = 0;
+    
+    //Inicio da parte paralela.
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
     
     while(1){
         
         //Encontra o próximo número em 'fibo'.
         mpz_add(fibo, ultm, penl);
         mpz_set(penl, ultm);
-        mpz_set(ultm, fibo);
+        mpz_set(ultm, fibo);        
+    
+        // Escolhe a thread para verificar a primaridade.
+        if((n_fib % size) == rank){
+            
+            //Testa se o número encontrado é primo.
+            if(mpz_probab_prime_p(fibo, PRECISAO)){
 
-        //Testa seu o número encontrado é primo.
-        if(mpz_probab_prime_p(fibo, PRECISAO)){
-            
-            //Imprime a hora que o número foi encontrado no arquivo.
-            system("DIA=$(date +%c) && echo [$DIA] >> resultado");
-  
-            //Imprime o número encontrado no arquivo.
-            resultado = fopen("resultado", "a");
-            mpz_out_str(resultado, 10, fibo);            
-            fputs("\n\n", resultado);
-            fclose(resultado);
-            
-            //Imprime a quantidade de números encontrados no terminal.
-            encontrados++;
-            printf("%dº Número encontrado\n", encontrados);
+                //Imprime a data e hora que o número foi encontrado no arquivo.
+                system("DIA=$(date +%c) && echo [$DIA] >> resultado2");
+
+                //Imprime o número encontrado, no arquivo.
+                resultado = fopen("resultado", "a");
+                mpz_out_str(resultado, 10, fibo);            
+                fputs("\n\n", resultado);
+                fclose(resultado);
+
+            }
             
         }
-      
+        
+        n_fib++;
     }
-   
+    
+    // 'Fim' da parte paralela
+    MPI_Finalize();
+
 }
